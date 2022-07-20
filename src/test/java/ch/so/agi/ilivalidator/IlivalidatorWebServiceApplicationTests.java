52a252f8f4984bda49b715e32e0c9a871201d173
edigonzales <edi.gonzales@gmail.com>
2022-07-20 08:01:16 +0200

    fix gh action

--- src/test/java/ch/so/agi/ilivalidator/IlivalidatorWebServiceApplicationTests.java
+++ src/test/java/ch/so/agi/ilivalidator/IlivalidatorWebServiceApplicationTests.java
@@ -2,10 +2,12 @@ package ch.so.agi.ilivalidator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @WebAppConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@ActiveProfiles("test")
 class IlivalidatorWebServiceApplicationTests {
 
 	@Test
